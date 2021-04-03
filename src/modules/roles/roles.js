5601a3d7bf7e0560cb5476384fe62e3c5db9c1d4
vxern <vxern@ctemplar.com>
2021-04-03 23:57:02 +0100

    Removing country roles

---
+++ src/modules/roles/roles.js
@@ -0,0 +1,18 @@
+export default
+{
+    "proficiency": [
+        "beginner", "intermediate", "advanced", "native"
+    ],
+    "regions": [
+        "ardeal", "banat", "bucovina", "crisana", "dobrogea", "maramures", "moldova", "muntenia", "oltenia", "transilvania"
+    ],
+    "ethnicity": [
+        "aromanian", "istroromanian", "meglenoromanian"
+    ],
+    "abroad": [
+        "diaspora"
+    ],
+    "extension": [
+        "voicechatter", "daily word"
+    ],
+}
