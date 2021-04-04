f7d80fde6d907aeff247950a152e25c36e52dea3
vxern <vxern@ctemplar.com>
2021-04-04 12:40:03 +0100

    Adding accent colours for warnings and errors

--- src/teacher/teacher_config.js
+++ src/teacher/teacher_config.js
@@ -3,5 +3,7 @@ export default
     "prefix": "mollie",
     "status": "Learning JavaScript",
 
-    "accentColor": 0x5c6bc0
+    "accentColorSuccess": 0x5c6bc0,
+    "accentColorWarning": 0xffff00,
+    "accentColorError": 0xff0000,
 }
