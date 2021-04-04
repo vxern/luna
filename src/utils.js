6b543650069911d7d3d52e66712e9d7a5fe746f9
vxern <vxern@ctemplar.com>
2021-04-04 12:39:48 +0100

    `capitaliseWords` fuinction that capitalises every word in a string; better documentation / comments

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,15 @@
+/// Capitalises each word in a string
+export function capitaliseWords(target) {
+    let terms = target.split(' ');
+    
+    for (let i = 0; i < terms.length; i++) {
+        terms[i] = terms[i][0].toUpperCase() + terms[i].substring(1);
+    }
+
+    return terms.join(' ');
+}
+
+/// Generates a random number between 0, inclusive and `max`, inclusive
 export function random(max) {
     return Math.floor(Math.random() * max);
 }
