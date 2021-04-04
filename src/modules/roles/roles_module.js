f04bf5033b4fd8d9b3dc75314714dbdac1475830
vxern <vxern@ctemplar.com>
2021-04-04 12:47:24 +0100

    Functionality for removing a role from a user

--- src/modules/roles/roles_module.js
+++ src/modules/roles/roles_module.js
@@ -1,7 +1,6 @@
-import { GuildMember } from 'discord.js';
 import { TeacherClient } from '../../teacher/teacher';
-import { TeacherModule } from '../module';
 import { roles } from './roles';
+import '../../utils';
 
 // Concatenate the roles specified in roles.js into a single array
 const allRoles = [].concat(
@@ -19,7 +18,7 @@ export class RolesModule {
             return false;
         }
 
-        return await resolveRole(message.author, message.channel, message.content);
+        return await this.resolveRole(message.author, message.channel, message.content);
     }
 
     /// Match seeked role against a role specified in `allRoles`
@@ -27,16 +26,92 @@ export class RolesModule {
         // If the seeked role is not a proficiency
         if (!roles.proficiency.includes(targetRole)) {
             // If the user does not have a proficiency role
-            if (!userHasProficiency(user)) {
-                TeacherClient.sendEmbed(textChannel, message = 'In order to get any additional roles, you must first have a proficiency role.');
+            if (!this.userHasProficiency(user)) {
+                TeacherClient.sendEmbed(textChannel, message = 'In order to get any additional roles, you must first have a proficiency role');
                 return true;
             }
             
-            if (!userHasRole(user, targetRole)) {
+            // If the user already has the seeked role
+            if (this.userHasRole(user, targetRole)) {
+                // Remove the seeked role from the user
+                this.removeRole(user, targetRole);
 
+                let message = `You no longer have the role '${capitaliseWords(targetRole)}'`;
+                
+                if (roles.regions.includes(targetRole)) {
+                    message = `You are no longer from ${capitaliseWords(targetRole)}`;
+                }
+
+                if (roles.ethnicity.includes(targetRole)) {
+                    message = `You are no longer from ${capitaliseWords(targetRole)}`;
+                }
+
+                if (roles.abroad.includes(targetRole)) {
+                    message = `You are no longer a ${capitaliseWords(targetRole) + 'n'}`;
+                }
+
+                TeacherClient.sendEmbed(textChannel, message = `:sob: ${message} :sob:`);
+                return true;
             }
+
+            let message = `You now have the role '${capitaliseWords(targetRole)}.'`;
             
-            
+            if (roles.regions.includes(targetRole)) {
+                if (this.userHasEnoughRegions(user)) {
+                    TeacherClient.sendWarning(textChannel, message = `A user may not have more than ${roles.maximumRegions} region roles.`)
+                    return true;
+                }
+            }
+
+                let message = '';
+                if (roles.roles_general.includes(target_role)) {
+                    message = `You now have the role '${language.capitalise(target_role)}'`;
+                } else if (roles.roles_regions.includes(target_role)) {
+                    // Check if user already has two region roles
+                    if (userHasEnoughRegions(user)) {
+                        text_channel.send({
+                            embed: {
+                                color: color, 
+                                description: `:exclamation: You may only have two region roles.`
+                            }
+                        });
+                        return;
+                    }
+                    message = `You are now from ${language.capitalise(target_role)}`;
+                } else if (roles.roles_countries.includes(target_role)) {
+                    // Check if user already has two country roles
+                    if (userHasEnoughCountries(user)) {
+                        text_channel.send({
+                            embed: {
+                                color: color, 
+                                description: `:exclamation: You may only have two country roles.`
+                            }
+                        });
+                        return;
+                    }
+                    message = `You are now from ${language.capitalise(target_role)}`;
+                } else if (roles.roles_ethnicity.includes(target_role)) {
+                    // Check if user already has an ethnicity
+                    if (userHasEthnicity(user)) {
+                        text_channel.send({
+                            embed: {
+                                color: color, 
+                                description: `:exclamation: You may only be of one Romanian ethnicity.`
+                            }
+                        });
+                        return;
+                    }
+                    message = `You are now ${language.capitalise(target_role)}`;
+                } else if (roles.roles_abroad.includes(target_role)) {
+                    message = `You are now a ${language.capitalise(target_role) + 'n'}`;
+                }
+                addRole(user, target_role);
+                text_channel.send({
+                    embed: {
+                        color: color, 
+                            description: `:partying_face: ${message} :partying_face:`
+                    }
+                });
         }
         
 
@@ -79,7 +154,7 @@ export class RolesModule {
     userHasEnoughRegions(user) {
         return user.roles.cache.filter(
             (role) => roles.regions.includes(role.name.toLowerCase())
-        ).size > 1;
+        ).size >= roles.maximumRegions;
     }
 
     /// Check if the user already has an ethnicity role
