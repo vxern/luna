072f554d47a7c2134a47b0f4e7e7e0a1bbeb9e78
vxern <vxern@ctemplar.com>
2021-04-04 23:51:07 +0100

    Fixing imports, adjusting configs, adding the RolesModule to `teacherModules`, fixing optional parameters using object destructuring

--- src/teacher/teacher.js
+++ src/teacher/teacher.js
@@ -1,23 +1,27 @@
 import { Client as DiscordClient } from 'discord.js';
 
 // Teacher modules
-import { RolesModule } from '../modules/roles/roles';
+import { RolesModule } from '../modules/roles/roles_module.js';
 /*
 import GameModule from '../modules/game/game';
 import MusicModule from '../modules/music/music';
 import WordChainModule from '../modules/word_chain/word_chain';
 */
 
+import { removeNonAlphanumeric } from '../language.js';
+
 // Teacher config
-import { channels } from '../config/channels.js';
-import { config } from './teacher_config.js';
+import * as channels from '../config/channels.js';
+import * as config from './teacher_config.js';
 
 const Client = new DiscordClient();
 
 export class TeacherClient {
     constructor() {
         // Modules used by teacher
-        this.teacherModules = [];
+        this.teacherModules = [
+            new RolesModule(),
+        ];
 
         Client.on('ready', () => {
             Client.user.setStatus(config.status);
@@ -44,27 +48,32 @@ export class TeacherClient {
             return;
         }
 
-        // Convert the content of the message to lowercase and trim the left side
-        message.content = message.content.toLowerCase().trimLeft().trimRight();
-
-        // If the message does not begin with the specified prefix
-        if (!message.content.startsWith(config.prefix) && !channels.unprefixedChannels.includes(message.channel.name)) {
+        if (channels.default.excludedChannels.includes(removeNonAlphanumeric(message.channel.name))) {
             return;
         }
 
-        message.content = message.content.replace(config.prefix, '').trimLeft();
+        // Convert the content of the message to lowercase and trim the left side
+        message.content = message.content.toLowerCase().trim();
+
+        // If the message does not begin with the specified prefix
+        if (!message.content.startsWith(config.default.prefix) && !channels.default.unprefixedChannels.includes(message.channel.name)) {
+            return;
+        }
+
+        // Remove the prefix from a string
+        message.content = message.content.replace(config.default.prefix, '').trimLeft();
 
         // Iterate over modules to find the suitable message handler
-        for (const teacherModule in this.teacherModules) {
+        this.teacherModules.forEach(async (teacherModule) => {
             // If the message has been handled, return
             if (await teacherModule.handleMessage(message)) {
                 return;
             }
-        }
+        });
     }
 
     /// Sends an embed to the text channel specified
-    static sendEmbed(textChannel, message = undefined, fields = undefined, color = config.accentColorSuccess) {
+    static async sendEmbed(textChannel, {message = undefined, fields = undefined, color = config.default.accentColorSuccess}) {
         if (fields === undefined) {
             if (message === undefined) {
                 console.error('Refused to send embed: Neither fields nor a message have been supplied into the embed.');
@@ -84,13 +93,13 @@ export class TeacherClient {
     }
 
     /// Sends an embed with a warning message
-    static sendWarning(textChannel, message = undefined, fields = undefined) {
-        this.sendEmbed(textChannel, message = message !== undefined ? `:warning: ${message}` : message, fields = fields, color = config.accentColorWarning);
+    static async sendWarning(textChannel, {message = undefined, fields = undefined}) {
+        this.sendEmbed(textChannel, {message: message !== undefined ? `:warning: ${message}` : message, fields: fields, color: config.default.accentColorWarning});
     }
 
     /// Sends an embed with an error message
-    static sendError(textChannel, message = undefined, fields = undefined) {
-        this.sendEmbed(textChannel, message = message !== undefined ? `:exclamation: ${message}` : message, fields = fields, color = config.accentColorError);
+    static async sendError(textChannel, message = undefined, fields = undefined) {
+        this.sendEmbed(textChannel, {message: message !== undefined ? `:exclamation: ${message}` : message, fields: fields, color: config.default.accentColorError});
     }
 }
 
