8eaf88290e937fa668500d513b97ef79e2d018ba
vxern <vxern@ctemplar.com>
2021-04-06 11:23:56 +0100

    `filterMap` function

--- src/utils.js
+++ src/utils.js
@@ -2,3 +2,13 @@
 export function random(max) {
     return Math.floor(Math.random() * max);
 }
+
+/// Maps over a.. map and removes the undesired elements
+export function filterMap(target, predicate) {
+    for (let key of target.keys()) {
+        if (!predicate) {
+            target.delete(key);
+        }
+    }
+    return target;
+}
