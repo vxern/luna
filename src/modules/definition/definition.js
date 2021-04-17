b5ce5fb99b0ffbf3a22470bec5eb7a8f6fc10428
Dorian Oszczęda <vxern@ctemplar.com>
2021-04-17 12:46:05 +0100

    Adding glosbe URL with selectors for translation elements

--- src/modules/definition/definition.js
+++ src/modules/definition/definition.js
@@ -1,6 +1,11 @@
 export default
 {
-    'dexonlineURL': 'https://www.dexonline.ro/definitie/{word}/json',
+    'glosbeURL': 'https://en.glosbe.com/en/ro/{word}',
+    'glosbeSelectorMainList': 'ul.translations__list',
+    'glosbeSelectorMainTranslations': 'li > div.flex.items-start > div > span.translation__item__phrase > h3 > span',
+    'glosbeSelectorExpandedList': 'div.less-frequent-translations > div.px-2.text-sm.font-medium.less-frequent-translations__list-compact',
 
-    'maximumDefinitions': 5,
+    'dexonlineURL': 'https://dexonline.ro/definitie/{word}/json',
+
+    'maximumTranslations': 3,
 }
