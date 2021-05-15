a0675f8a89ce9f4e3eff33cc765994fe5a3634a0
Dorian Oszczęda <vxern@ctemplar.com>
2021-05-15 12:25:37 +0100

    Adding cooldown functionality for user thanks

--- src/fauna/database.js
+++ src/fauna/database.js
@@ -1,6 +1,9 @@
 import { default as fauna } from 'faunadb';
+import { default as moment} from 'moment';
 
 import { TeacherClient } from '../teacher/teacher.js';
+
+import * as config from './database_config.js';
 
 const q = fauna.query;
 
@@ -18,7 +21,9 @@ export class FaunaDatabase {
         const response = await this.dispatchQuery(
             q.Create(q.Collection('Users'), {
                 data: {
-                    userID: userId,
+                    userId: userId,
+                    // This array will contain the IDs and timestamps of when a user was thanked
+                    lastThanked: {},
                     thanks: 0,
                     activityPoints: 0,
                 }
@@ -52,8 +57,6 @@ export class FaunaDatabase {
 
     /// Queries Fauna and returns the requested user's stats or otherwise creates the user entry if one does not exist
     async getUserInformation(userId) {
-        console.log(this.userCache.has(userId));
-
         if (this.userCache.has(userId)) {
             return this.userCache.get(userId);
         }
@@ -63,38 +66,58 @@ export class FaunaDatabase {
         );
 
         if (userInformation === 'instance not found') {
-            userInformation = await this.createUserEntry(userId);
+            return await this.createUserEntry(userId);
         }
-
-        this.userCache.set(userId, userInformation);
-        console.log(this.userCache.has(userId));
         
         return userInformation;
     }
 
     /// Increments the 'thanks' count of a user
-    async thankUser(textChannel, userId) {
-        const user = await this.getUserInformation(userId);
-
-        if (user === undefined) {
+    async thankUser(textChannel, originUserId, targetUserId) {
+        const targetUser = await this.getUserInformation(targetUserId);
+        const originUser = await this.getUserInformation(originUserId);
+   
+        // If either user has not been fetched correctly
+        if (targetUser === undefined || originUser === undefined) {
             TeacherClient.sendError(textChannel, {
-                message: `Couldn't fetch data of user #${userId}`,
+                message: `Couldn't fetch data of user #${targetUser === undefined ? targetUserId : originUserId}.`,
             });
-            return;
+            return false;
         }
 
+        // Get the time difference between now and the time the thank was submitted for the user
+        const now = moment();
+        const then = moment.unix(originUser.data.lastThanked[targetUserId]);
+        const hourDifference = now.diff(then, 'hours');
+
+        // Checks if the origin user is eligible for thanking the target user by checking if the origin user has already thanked
+        // them in the specified time frame, and if the origin user does not have the target user in their [lastThanked] map.
+        const isEligibleToVote = 
+            (isNaN(hourDifference) ? true : hourDifference >= config.default.thankCooldown) && 
+            !(targetUserId in originUser.data.lastThanked);
+
+        if (!isEligibleToVote) {
+            TeacherClient.sendWarning(textChannel, {
+                message: `You must wait ${config.default.thankCooldown} hours in order to thank the same person again.`,
+            });
+            return false;
+        }
+        
+        // Add the target user's id as well as the timestamp to the [lastThanked] object
+        originUser.data.lastThanked[targetUserId] = now.unix();
+
+        // Update both users' entries
         const response = await this.dispatchQuery(
-            q.Update(user.ref, {data: {thanks: user.data.thanks += 1}})
+            q.Do([
+                q.Update(targetUser.ref, {data: {thanks: targetUser.data.thanks += 1}}),
+                q.Update(originUser.ref, {data: {lastThanked: originUser.data.lastThanked}})
+            ])
         );
+        
+        this.userCache.set(targetUserId, response[0]);
+        this.userCache.set(originUserId, response[1]);
 
-        if (response.data === undefined) {
-            TeacherClient.sendError(textChannel, {
-                message: `Couldn't update data of user #${userId}`,
-            });
-            return;
-        }
-
-        this.userCache.set(userId, response);
+        return true;
     }
 
     async dispatchQuery(query) {
