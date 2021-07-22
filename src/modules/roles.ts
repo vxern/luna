f59534c12ccfc16daeab5c137b4fe6c9f18863b0
vxern <vxern@wordcollector.co.uk>
2021-07-22 23:37:21 +0100

    Added roles module with assignment and removal of roles

---
+++ src/modules/roles.ts
@@ -0,0 +1,232 @@
+import { EmbedField, GuildMember, Role } from 'discord.js';
+
+import { MynaModule } from './module';
+import { MynaClient } from '../client/client';
+import { Embed } from '../structs/embed';
+
+import { Language } from '../language';
+
+import roles from '../roles.json';
+
+// Concatenate the roles specified in [roles.json] into a single array
+const allRoles: Array<string> = ([] as Array<string>).concat(
+  roles.proficiency,
+  roles.regions,
+  roles.ethnicities,
+  roles.abroad,
+  roles.miscellaneous,
+);
+
+export class RolesModule extends MynaModule {
+  commandTree = {
+    'roles': () => this.displayRoles(),
+    '%roleName': (roleName: string) => this.resolveRole(roleName),
+  };
+
+  displayRoles() {
+    let fields: Array<EmbedField> = [
+      {
+        name: 'Proficiency',
+        value: roles.proficiency.join(', '),
+        inline: true,
+      },
+    ]
+
+    if (this.hasProficiency()) {
+      fields.push(
+        ...Object.entries(roles).map<EmbedField>(([key, value]) => {
+          return {
+            name: key,
+            value: (value as string[]).join(', '),
+            inline: true,
+          }
+        })
+      );
+    }
+
+    MynaClient.info(this.args['textChannel'], new Embed({fields: fields}));
+  }
+
+  async resolveRole(roleName: string): Promise<boolean> {
+    roleName = roleName.toLowerCase();
+
+    // If the sought role is not found in [allRoles]
+    if (!allRoles.includes(roleName)) {
+      return false;
+    }
+
+    // If the sought role is not a proficiency role
+    if (!roles.proficiency.includes(roleName)) {
+      return await this.resolveNonProficiencyRole(roleName);
+    }
+
+    if (!this.hasRole(roleName)) {
+      const currentProficiencyRole = this.getCurrentProficiencyRole();
+      return await this.addOrReplaceProficiencyRole(roleName, currentProficiencyRole);
+    }
+
+    let message = `Your level is already ${roleName} and you may instead `;
+    let upgradeMessage = ':arrow_double_up: upgrade to ';
+    let downgradeMessage = ':arrow_double_down: downgrade to ';
+
+    const indexOfProficiencyRole = roles.proficiency.indexOf(roleName);
+
+    const roleTagsToUpgradeTo: Array<string> = this.getTagsOfProficiencyRolesBetterThan(indexOfProficiencyRole);
+    const roleTagsToDowngradeTo: Array<string> = this.getTagsOfProficiencyRolesWorseThan(indexOfProficiencyRole);
+
+    if (roleTagsToUpgradeTo.length !== 0) {
+      upgradeMessage += Language.join(roleTagsToUpgradeTo);
+      message += upgradeMessage;
+
+      if (roleTagsToDowngradeTo.length !== 0) {
+        message += ' or ';
+      }
+    }
+
+    if (roleTagsToDowngradeTo.length !== 0) {
+      downgradeMessage += Language.join(roleTagsToDowngradeTo);
+      message += downgradeMessage;
+    }
+
+    MynaClient.info(this.args['textChannel'], new Embed({message: message}));
+    return true;
+  }
+
+  async resolveNonProficiencyRole(roleName: string): Promise<boolean> {
+    // If the user does not have a proficiency role yet
+    if (!this.hasProficiency()) {
+      return true;
+    }
+
+    // If the user already has the sought role
+    if (this.hasRole(roleName)) {
+      return await this.removeRole(roleName);
+    }
+
+    return await this.addRole(roleName);
+  }
+
+  async addRole(roleName: string): Promise<boolean> {
+    const member: GuildMember = this.args['member'];
+    await member.roles.add(this.findRole(roleName));
+
+    const capitalisedRoleName = Language.capitaliseWords(roleName);
+
+    let message = `You now have the role '${capitalisedRoleName}'`;
+
+    if (roles.ethnicities.includes(roleName)) {
+      if (this.hasEnoughEthnicityRoles()) {
+        MynaClient.warn(this.args['textChannel'], new Embed({
+          message: `You may not be of more than ${roles.maximumEthnicityRoles} Romanian ethnicities at any given time`,
+        }));
+        return true;
+      }
+
+      message = `You are now ${capitalisedRoleName}`;
+    }
+
+    if (roles.regions.includes(roleName)) {
+      if (this.hasEnoughRegionRoles()) {
+        MynaClient.warn(this.args['textChannel'], new Embed({
+          message: `You may not have more than ${roles.maximumRegionRoles} region roles at any given time`,
+        }));
+        return true;
+      }
+
+      message = `You are now from ${capitalisedRoleName}`;
+    }
+
+    if (roles.abroad.includes(roleName)) {
+      message = `You are now a ${roleName}`;
+    }
+
+    MynaClient.info(this.args['textChannel'], new Embed({message: `:partying_face: ${message} :partying_face:`}));
+    return true;
+  }
+
+  async removeRole(roleName: string): Promise<boolean> {
+    const member: GuildMember = this.args['member'];
+    await member.roles.remove(this.findRole(roleName));
+
+    const capitalisedRoleName = Language.capitaliseWords(roleName);
+
+    let message = `You no longer have the role '${capitalisedRoleName}'`;
+
+    if (roles.regions.includes(roleName)) {
+      message = `You are no longer from ${capitalisedRoleName}`;
+    }
+
+    if (roles.ethnicities.includes(roleName)) {
+      message = `You are no longer ${capitalisedRoleName}`;
+    }
+
+    if (roles.abroad.includes(roleName)) {
+      message = `You are no longer a ${capitalisedRoleName}`;
+    }
+
+    MynaClient.info(this.args['textChannel'], new Embed({message: `:sob: ${message} :sob:`}));
+    return true;
+  }
+
+  async addOrReplaceProficiencyRole(roleName: string, currentProficiencyRole: Role | undefined): Promise<boolean> {
+    const member: GuildMember = this.args['member'];
+    await member.roles.add(this.findRole(roleName));
+
+    if (currentProficiencyRole !== undefined) {
+      await this.removeProficiencyRole(currentProficiencyRole);
+    }
+
+    MynaClient.info(this.args['textChannel'], new Embed({message: `Your level is now ${roleName}`}));
+    return true;
+  }
+
+  async removeProficiencyRole(proficiencyRole: Role): Promise<boolean> {
+    const member: GuildMember = this.args['member'];
+    await member.roles.remove(proficiencyRole);
+    return true;
+  }
+
+  findRole(roleName: string): Role {
+    const member: GuildMember = this.args['member'];
+    return member.guild.roles.cache.find((role) => role.name.toLowerCase() === roleName)!;
+  }
+
+  hasRole(roleName: string): boolean {
+    const member: GuildMember = this.args['member'];
+    return member.roles.cache.some((role) => role.name.toLowerCase() === roleName);
+  }
+
+  hasProficiency(): boolean {
+    const member: GuildMember = this.args['member'];
+    return member.roles.cache.some((role) => roles.proficiency.includes(role.name.toLowerCase()))
+  }
+
+  getCurrentProficiencyRole(): Role | undefined {
+    const member: GuildMember = this.args['member'];
+    return member.guild.roles.cache.find(
+      (role) => roles.proficiency.includes(role.name.toLowerCase())
+    );
+  }
+
+  getTagsOfProficiencyRolesBetterThan(roleIndex: number) {
+    return roles.proficiency.filter((_, index) => index > roleIndex).map((roleName) => this.findRole(roleName).id);
+  }
+
+  getTagsOfProficiencyRolesWorseThan(roleIndex: number) {
+    return roles.proficiency.filter((_, index) => index < roleIndex).map((roleName) => this.findRole(roleName).id);
+  }
+
+  hasEnoughEthnicityRoles(): boolean {
+    const member: GuildMember = this.args['member'];
+    return member.roles.cache.filter(
+      (role) => roles.regions.includes(role.name.toLowerCase())
+    ).size >= roles.maximumEthnicityRoles;
+  }
+
+  hasEnoughRegionRoles(): boolean {
+    const member: GuildMember = this.args['member'];
+    return member.roles.cache.filter(
+      (role) => roles.regions.includes(role.name.toLowerCase())
+    ).size >= roles.maximumRegionRoles;
+  }
+}
