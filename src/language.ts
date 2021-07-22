e3de8fb0ecfcdbcef4f5356f53a786c4374f1131
vxern <vxern@wordcollector.co.uk>
2021-07-22 23:31:35 +0100

    `capitaliseWords` helper method

--- src/language.ts
+++ src/language.ts
@@ -1,4 +1,8 @@
 export class Language {
+  static capitaliseWords(target: string) {
+    return target.split(' ').map((word) => word[0].toUpperCase() + word.substring(1)).join(' ');
+  }
+
   static join(array: Array<String>, operator: 'and' | 'or' = 'and') {
     const lastElement = array.pop();
     let joined = array.join(', ');
