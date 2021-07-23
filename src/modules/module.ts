6fccd17c6473c3b388f59a9caf2e64f7d0b99044
vxern <vxern@wordcollector.co.uk>
2021-07-23 18:19:23 +0100

    Added [requirement] field for a required check to pass before commands can match to a branch in the command tree

--- src/modules/module.ts
+++ src/modules/module.ts
@@ -1,7 +1,8 @@
-export type Handler = (args: Array<any>) => Promise<boolean>
+export type Handler = (args?: any[]) => Promise<boolean>
 
 export abstract class MynaModule {
   [functionName: string]: Handler | Object;
+  requirement: Handler | boolean = true;
   abstract commandTree: Object;
   args: any;
 }
