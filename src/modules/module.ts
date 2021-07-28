342f62ae01dfaff2445bd97691d1ffbf2fd7a4f9
vxern <vxern@wordcollector.co.uk>
2021-07-28 20:02:33 +0100

    `unimplemented` function and readonly modifier on class variables

--- src/modules/module.ts
+++ src/modules/module.ts
@@ -1,8 +1,19 @@
+import { LunaClient } from "../client/client";
+import { Embed } from "../structs/embed";
+
 export type Handler = (args?: any[]) => Promise<boolean>
 
 export abstract class LunaModule {
   [functionName: string]: Handler | Object;
-  requirement: Handler | boolean = true;
-  abstract commandTree: Object;
+  readonly requirement: Function | boolean = true;
+  readonly beforeExecutingCommand: Function = () => {};
+  abstract readonly commandTree: Object;
   args: any;
+
+  async unimplemented(): Promise<boolean> {
+    LunaClient.error(this.args['textChannel'], new Embed({
+      message: 'This function is not yet implemented',
+    }));
+    return true;
+  }
 }
