decc0ade9ad7b775e7810f0c1ceb23f2a11c48b2
vxern <vxern@wordcollector.co.uk>
2021-07-28 20:03:12 +0100

    Renamed `getPairs` to `getKeywordPairs`

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 export class Utils {
-  static getPairs(array: string[], set: 'even' | 'odd'): string[] {
+  static getKeywordPairs(array: string[], set: 'even' | 'odd'): string[] {
     const indexes = [...Array(array.length).keys()];
     const inSet = set === 'even' ? (index: number) => index % 2 === 0 : (index: number) => index % 2 !== 0;
     return indexes.filter(inSet).map((index) => array[index] + ' ' + array[index + 1])
