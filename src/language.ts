40c8b1476dcd54142a3e9eab469a3baed373e505
vxern <vxern@wordcollector.co.uk>
2021-07-28 20:00:17 +0100

    Function to find and highlight matched keywords

--- src/language.ts
+++ src/language.ts
@@ -1,6 +1,29 @@
+import { distance } from 'fastest-levenshtein';
+import { Utils } from './utils';
+
 export class Language {
   static capitaliseWords(target: string) {
     return target.split(' ').map((word) => word[0].toUpperCase() + word.substring(1)).join(' ');
+  }
+
+  static highlightKeywords(target: string, phrase: string) {
+    // Extract keywords which should be highlighted in [target] from [phrase] in lowercase format
+    const keywordsToHighlight = phrase.toLowerCase().split(' ');
+    // Lambda function calculating the maximum acceptable distance for two words be determined to be similar
+    const acceptableDistance = (word: string) => Math.max(Math.round(word.length / Math.E), 1);
+    // Lambda function determining whether a word is similar to a keyword in [keywordsToHighlight]
+    const similar = (word: string) => keywordsToHighlight.some((keyword) => distance(keyword, word.toLowerCase()) <= acceptableDistance(keyword));
+    // Get the string keywords without unnecessary symbols
+    const alphanumericOnlyKeywords = this.removeNonAlphanumeric(target).split(' ');
+    // Find those words which when in lowercase format are similar to any of [keywordsToHighlight]
+    const keywordsFound = [
+      ...alphanumericOnlyKeywords.filter(similar), 
+      ...Utils.getKeywordPairs(alphanumericOnlyKeywords, 'even').filter(similar), 
+      ...Utils.getKeywordPairs(alphanumericOnlyKeywords, 'odd').filter(similar),
+    ];
+    // Highlight the necessary keywords in [target]
+    keywordsFound.forEach((keyword) => {target = target.replace(RegExp(keyword, 'g'), '**' + keyword + '**')});
+    return target;
   }
 
   static join(array: string[], operator: 'and' | 'or' = 'and') {
@@ -16,7 +39,11 @@ export class Language {
     return joined;
   }
 
+  static normaliseSpaces(target: string) {
+    return target.trim().replace(/ +/g, ' ')
+  }
+
   static removeNonAlphanumeric(target: string) {
-    return target.replace(/\W/g, '');
+    return this.normaliseSpaces(target.replace(/[^0-9a-zA-Z_ ]/g, ''));
   }
 }
