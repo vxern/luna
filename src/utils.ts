6a9de728f2b7eef6a0802a9ed8e177733d7be2e5
vxern <vxern@wordcollector.co.uk>
2021-07-28 18:05:35 +0100

    Function to get keyword pairs from an array of strings

---
+++ src/utils.ts
@@ -0,0 +1,7 @@
+export class Utils {
+  static getPairs(array: string[], set: 'even' | 'odd'): string[] {
+    const indexes = [...Array(array.length).keys()];
+    const inSet = set === 'even' ? (index: number) => index % 2 === 0 : (index: number) => index % 2 !== 0;
+    return indexes.filter(inSet).map((index) => array[index] + ' ' + array[index + 1])
+  }
+}
