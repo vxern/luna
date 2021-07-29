46a3d97c3683ba1c9680a2f5d5335eb473d85a8e
vxern <vxern@wordcollector.co.uk>
2021-07-29 11:49:15 +0100

    Updated documentation

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 import { LunaClient } from './client/client';
 
-const Client: LunaClient = new LunaClient();
-
 const requiredEnv = ['DISCORD_SECRET', 'FAUNA_SECRET', 'YOUTUBE_SECRET'];
-const presentEnv = requiredEnv.map((value) => value in process.env);
+/// Array of booleans indicating which environment variables are present at launch
+const presentEnv = requiredEnv.map((env) => env in process.env);
 
-// If there is an environment variable not present
+// If at least one environment variable is not present
 if (presentEnv.includes(false)) {
-  const notPresent = (value: boolean) => !value;
-  const notPresentEnv = presentEnv.filter(notPresent).map((_, index) => requiredEnv[index]);
-  console.error(`Missing one or more required environment variables: ${notPresentEnv.join(', ')}`);
+  const missingEnv = presentEnv.filter((env: boolean) => !env).map((_, index) => requiredEnv[index]);
+  console.error(`Missing one or more required environment variables: ${missingEnv.join(', ')}`);
   process.exit(1);
 }
+
+const Client: LunaClient = new LunaClient();
 
 async function main() {
   await Client.login();
