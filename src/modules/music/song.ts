6f794fbc8428625d6e976c7ea8fe304d90f67e0a
vxern <vxern@wordcollector.co.uk>
2021-07-29 20:34:33 +0100

    Added [duration] and `fromDetails()` + `runningTimeAsString()` functions

--- src/modules/music/song.ts
+++ src/modules/music/song.ts
@@ -1,16 +1,36 @@
+import { MoreVideoDetails } from "ytdl-core";
+
+import { Language } from "../../language";
+
 export class Song {
   /// Song title
   title: string;
   /// Song URL
   url: string;
-  /// Describes the song offset in seconds
+  /// Song offset in seconds
   offset: number = 0;
+  /// Song span/length in seconds
+  duration: number = 0;
   /// Only users present when this song was requested can manage it
   canBeManagedBy: string[];
 
-  constructor({title, url, canBeManagedBy = []}: {title: string, url: string, canBeManagedBy?: string[]}) {
+  constructor({title, url, length, canBeManagedBy = []}: {title: string, url: string, length: number, canBeManagedBy?: string[]}) {
     this.title = title;
     this.url = url;
+    this.duration = length;
     this.canBeManagedBy = canBeManagedBy;
+  }
+
+  runningTimeAsString(): string {
+    return Language.secondsToExtendedFormat(this.offset) + ' / ' + Language.secondsToExtendedFormat(this.duration);
+  }
+
+  static fromDetails(videoDetails: MoreVideoDetails, usersPresent: string[]) {
+    return new Song({
+      title: videoDetails.title,
+      url: videoDetails.video_url,
+      length: Number(videoDetails.lengthSeconds),
+      canBeManagedBy: usersPresent,
+    })
   }
 }
