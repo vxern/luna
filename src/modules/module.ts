4664b4edee06c0af99588a4a6da9236626a1bffe
vxern <vxern@wordcollector.co.uk>
2021-07-29 11:59:24 +0100

    Added `resolveTimeQuery` function to resolve a duration query to the number of seconds

--- src/modules/module.ts
+++ src/modules/module.ts
@@ -1,5 +1,10 @@
+import { TextChannel } from "discord.js";
+
 import { LunaClient } from "../client/client";
 import { Embed } from "../client/embed";
+
+const digits = /\d+/g;
+const words = /[a-zA-Z]+/g;
 
 export abstract class LunaModule {
   [functionName: string]: Function | Object;
@@ -13,5 +18,60 @@ export abstract class LunaModule {
       message: 'This function is not yet implemented',
     }));
     return true;
+  }
+  
+  /// Parses a time query to a base number of seconds described by the query
+  resolveTimeQuery(query: string): number | undefined {
+    let seconds = 0;
+
+    // Extract the digits present in the query
+    let integers = query.match(digits)?.map((integer) => parseInt(integer)) || [];
+    // Extract the strings present in the query
+    let strings = query.match(words) || [];
+
+    // No parameters provided for either keys or values
+    if (integers === null || strings === null) {
+      LunaClient.warn(this.args['textChannel'], new Embed({
+        message: 'You have not provided a valid time description as one of the required terms is missing'
+      }));
+      return;
+    }
+
+    // The number of keys does not match the number of values
+    if (integers.length !== strings.length) {
+      LunaClient.warn(this.args['textChannel'], new Embed({
+        message: 'The number of time specifiers and values does not match'
+      }));
+      return;
+    }
+
+    const secondIdentifiers = ['s', 'sec', 'second', 'seconds'];
+    const minuteIdentifiers = ['m', 'min', 'minute', 'minutes'];
+    const hourIdentifiers = ['h', 'hr', 'hour', 'hours'];
+
+    for (let index = 0; index < integers.length; index++) {
+      let multiplier = 1;
+
+      if (secondIdentifiers.includes(strings[index])) {
+        continue;
+      }
+
+      if (minuteIdentifiers.includes(strings[index])) {
+        multiplier = 60;
+        continue;
+      }
+
+      if (hourIdentifiers.includes(strings[index])) {
+        multiplier = 60 * 60;
+        continue;
+      }
+
+      LunaClient.warn(this.args['textChannel'], new Embed({
+        message: `'${strings[index]} is not a valid time specifier'`,
+      }));
+      return;
+    }
+
+    return seconds;
   }
 }
