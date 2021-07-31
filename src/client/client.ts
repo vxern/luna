3355cb97b5688f7e55ffcd32402ee9f48499f8a7
vxern <vxern@wordcollector.co.uk>
2021-07-31 23:46:25 +0100

    Renamed `LunaClient` to Client, cleaned up implementation

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,40 +1,38 @@
-import { Client as DiscordClient, TextChannel, Message } from 'discord.js';
+import { Client as DiscordClient, ClientUser, TextChannel, Message } from 'discord.js';
+import * as string from 'string-sanitizer';
 
-import { LunaModule as LunaModule } from '../modules/module';
-import { MusicModule } from '../modules/music/music';
-import { RolesModule } from '../modules/roles';
-
-import { cyclePresence } from '../services/presence';
 import { Embed } from './embed';
-import { Language } from '../language';
+
+import { Module } from '../modules/module';
+import { Music } from '../modules/music/music';
+import { Roles } from '../modules/roles/roles';
+
+import { Service } from '../services/service';
+import { Presence } from '../services/presence';
+
+import { Utils } from '../utils';
 
 import config from '../config.json';
+import { Command } from '../modules/command';
 
-export class LunaClient {
-  private modules!: LunaModule[];
-  private client: DiscordClient;
+export class Client {
+  private readonly client: DiscordClient = new DiscordClient();
+  private modules: Module[] = Utils.instantiated([Music, Roles]);
+  private services: Service[] = Utils.instantiated([Presence]);
 
-  constructor() {
-    this.client = new DiscordClient();
-    
-    this.client.on('ready', () => this.initialize());
-  }
+  static bot: ClientUser;
 
-  private initialize() {
-    cyclePresence(this.client.user!);
-
+  /// Begin listening to events
+  async initialise() {
     this.client.on('message', (message) => this.handleMessage(message));
 
-    this.modules = [
-      new MusicModule(),
-      new RolesModule(),
-    ];
-
-    console.info(`${Language.capitaliseWords(config.alias)} is ready to serve with ${this.modules.length} modules.`);
-  }
-
-  async login() {
     await this.client.login(process.env.DISCORD_SECRET);
+
+    Client.bot = this.client.user!;
+
+    Utils.initialiseServices(this.services);
+
+    console.info(`Ready to serve with ${this.modules.length} modules and ${this.services.length} services.`);
   }
 
   private handleMessage(message: Message) {
@@ -43,7 +41,7 @@ export class LunaClient {
       return;
     }
 
-    // If the message was submitted by itself
+    // If the message was submitted by the bot itself
     if (message.member!.id === this.client.user!.id) {
       return;
     }
@@ -51,109 +49,69 @@ export class LunaClient {
     message.channel = message.channel as TextChannel;
 
     // If the message was submitted in an excluded channel
-    if (Language.removeNonAlphanumeric(message.channel.name) in config.excludedChannels) {
+    if (string.sanitize.keepUnicode(message.channel.name) in config.excludedChannels) {
       return;
     }
 
-    // Transform the message content into a digestible format
-    let messageTrimmed: string = Language.normaliseSpaces(message.content);
+    message.content = Utils.normaliseSpaces(message.content);
 
     const inAliaslessChannel = config.aliaslessChannels.includes(message.channel.name)
+    const messageStartsWithAlias = message.content.toLowerCase().startsWith(config.alias);
 
-    // If the message doesn't begin with the specified alias
-    if (!messageTrimmed.toLowerCase().startsWith(config.alias) && !inAliaslessChannel) {
+    if (!messageStartsWithAlias && !inAliaslessChannel) {
       return;
-    } 
+    }
 
-    // Remove the prefix to leave just the parsable content
-    const args = messageTrimmed.split(' ');
-    if (!inAliaslessChannel) {
-      args.shift();
-      message.content = args.join(' ');
-    } else {
-      message.content = messageTrimmed;
+    if (messageStartsWithAlias) {
+      message.content = Utils.removeFirstWord(message.content);
     }
 
     if (message.content.length === 0) {
-      LunaClient.info(message.channel, new Embed({
-        message: 'No command was provided',
-      }));
+      // TODO: this.help(message.channel);
       return;
     }
   
-    this.resolveMessageToCommand(message);
+    this.findCommandHandler(message);
   }
 
-  private resolveMessageToCommand(message: Message) {
-    const args = message.content.split(' ');
-    const foremostCommand = args[0];
+  private findCommandHandler(message: Message) {
+    const messageLowercase = message.content.toLowerCase();
 
-    const branchIsMatch = ([commandsWithTooltip, _]: [string, any]) => {
-      const commands = commandsWithTooltip
-        .split('~')[0]
-        .trimRight()
-        .split('|')
-        .map((command) => command.trim());
-      return commands[0].startsWith('$') || 
-        commands.includes(foremostCommand) ||
-        // A command can consist of two words, therefore it's necessary to check the whole content too 
-        commands.includes(message.content)
-    };
+    const commandMatchesQuery = (command: Command<Module>) => 
+      command.identifier.startsWith('$') || 
+      messageLowercase.startsWith(command.identifier) || 
+      command.aliases.some(
+        (alias) => messageLowercase.startsWith(alias)
+      );
 
-    // Find branches with a key corresponding to the foremost argument, with the [requirement]
-    // yielding true and concatenate them into a single array
-    const matchedBranches = ([] as [string, any][]).concat(...this.modules
-      // Get modules with a command tree whose branch' command matches the message
-      .filter((module) => Object.entries(module.commandTree).some(branchIsMatch))
-      // Ensure that the module's [requirement] is met, otherwise exclude its [commandTree]
-      .filter((module) => {
-        let requirementMet: boolean;
+    const matchedCommand = ([] as Command<Module>[]).concat(...this.modules
+      .filter((module) => module.requirementMet(message))
+      // Fetch the lists of commands
+      .map((module) => module.commands)
+      // Find the commands whise identifier or aliases match the message content
+      .map((commands) => commands.filter(commandMatchesQuery))
+    )[0] || undefined;
 
-        module.args = {
-          'textChannel': message.channel as TextChannel,
-          'voiceChannel': message.member?.voice.channel,
-          'member': message.member,
-          'bot': this.client.user,
-          'message': message,
-        }
-
-        if (typeof module.requirement === 'boolean') {
-          requirementMet = module.requirement;
-        } else {
-          requirementMet = module.requirement();  
-        }
-
-        if (requirementMet) {
-          module.beforeExecutingCommand();
-        }
-
-        return requirementMet;
-      })
-      // Find branches whose commands match the message
-      .map((module) => Object.entries(module.commandTree).filter(branchIsMatch))
-    );
-
-    message.channel = message.channel as TextChannel;
-
-    // If branches have been found corresponding to the command
-    if (matchedBranches.length === 0) {
-      LunaClient.warn(message.channel, new Embed({
-        message: 'Unknown command',
-      }));
+    if (matchedCommand === undefined) {
+      Client.warn(message.channel as TextChannel, 'Unknown command');
       return;
     }
-
-    const [command, callback] = matchedBranches[0];
-
-    if (!command.split(' ')[0].startsWith('$')) {
-      args.shift();
-    }
-    message.content = args.join(' ');
     
-    callback(message.content);
+    if (!matchedCommand.identifier.startsWith('$')) {
+      message.content = Utils.removeFirstWord(message.content);
+    }
+
+    const neededDependencies = Utils.getNamesOfDependencies(matchedCommand.dependencies);
+    const dependencies: [any, any][] = neededDependencies.map(
+      (dependency) => [dependency, matchedCommand.module.commands.find(
+        (command) => Utils.capitaliseWords(command.identifier) === dependency,
+      )]
+    );
+
+    matchedCommand.handler(message, new Map(dependencies));
   }
 
-  private static async sendEmbed(textChannel: TextChannel, embed: Embed): Promise<Message> {
+  static async send(textChannel: TextChannel, embed: Embed): Promise<Message> {
     return textChannel.send({embed: {
       title: embed.title,
       thumbnail: {url: embed.thumbnail},
@@ -163,31 +121,32 @@ export class LunaClient {
     }})
   }
 
-  static async tip(textChannel: TextChannel, embed: Embed): Promise<Message> {
-    if (embed.message !== undefined) {
-      embed.message = `:information_source: ` + embed.message;
-    }
-    embed.color = config.accentColorTip;
-    return this.sendEmbed(textChannel, embed);
+  /// Send an embedded message with a tip
+  static async tip(textChannel: TextChannel, message: string): Promise<Message> {
+    return this.send(textChannel, new Embed({
+      message: `:information_source: ` + message,
+      color: config.accentColorTip,
+    }));
   }
 
-  static async info(textChannel: TextChannel, embed: Embed): Promise<Message> {
-    return this.sendEmbed(textChannel, embed);
+  /// Send an embedded message with an informational message
+  static async info(textChannel: TextChannel, message: string): Promise<Message> {
+    return this.send(textChannel, new Embed({message: message}));
   }
 
-  static async warn(textChannel: TextChannel, embed: Embed): Promise<Message> {
-    if (embed.message !== undefined) {
-      embed.message = `:warning: ` + embed.message;
-    }
-    embed.color = config.accentColorWarning;
-    return this.sendEmbed(textChannel, embed);
+  /// Send an embedded message with a warning
+  static async warn(textChannel: TextChannel, message: string): Promise<Message> {
+    return this.send(textChannel, new Embed({
+      message: `:warning: ` + message,
+      color: config.accentColorWarning,
+    }));
   }
 
-  static async error(textChannel: TextChannel, embed: Embed): Promise<Message> {
-    if (embed.message !== undefined) {
-      embed.message = `:exclamation: ` + embed.message;
-    }
-    embed.color = config.accentColorError;
-    return this.sendEmbed(textChannel, embed);
+  /// Send an embedded message with an error
+  static async error(textChannel: TextChannel, message: string): Promise<Message> {
+    return this.send(textChannel, new Embed({
+      message: `:exclamation: ` + message,
+      color: config.accentColorError,
+    }));
   }
 }
