904ccc36671fda296d1ac32df4934a213214b3c0
vxern <vxern@wordcollector.co.uk>
2021-07-31 23:45:12 +0100

    Updated implementation of the roles module

---
+++ src/modules/roles/roles.ts
@@ -0,0 +1,39 @@
+import { GuildMember, Role } from 'discord.js';
+
+import { Module } from '../module';
+
+import { Assignable } from './commands/assignable';
+import { AssignRole } from './commands/assign-role';
+
+import { Utils } from '../../utils';
+
+import roles from '../../roles.json';
+
+export class Roles extends Module {
+  readonly commands = Utils.instantiated([Assignable, AssignRole], [this]);
+
+  // Concatenate the roles specified in [roles.json] into a single array
+  readonly allRoles = [
+    ...roles.proficiency,
+    ...roles.regions,
+    ...roles.ethnicities,
+    ...roles.abroad,
+    ...roles.miscellaneous,
+  ];
+
+  /// Resolve the name of a role to a `Role` object
+  findRole(member: GuildMember, roleName: string): Role {
+    return member.guild.roles.cache.find((role) => role.name.toLowerCase() === roleName)!;
+  }
+
+  /// Check if the user has a proficiency role by passing its name
+  hasProficiency(member: GuildMember): boolean {
+    return member.roles.cache.some((role) => roles.proficiency.includes(role.name.toLowerCase()))
+  }
+
+  /// Convert a role's ID to a Discord in-message tag
+  toTag(id: string): string {
+    return `<@&${id}>`;
+  }
+
+}
