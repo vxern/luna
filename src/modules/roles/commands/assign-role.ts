ae033d4956835bbcd1d03004e196e2f638ecf046
vxern <vxern@wordcollector.co.uk>
2021-07-31 23:38:37 +0100

    Extracted role commands into their monolithic data representations

---
+++ src/modules/roles/commands/assign-role.ts
@@ -0,0 +1,194 @@
+import { GuildMember, Message, Role, TextChannel } from "discord.js";
+
+import { Roles } from "../roles";
+import { Command } from "../../command";
+
+import roles from '../../../roles.json';
+import { Utils } from "../../../utils";
+import { Client } from "../../../client/client";
+
+export class AssignRole extends Command<Roles> {
+  readonly identifier = '$roleName';
+  readonly aliases = [];
+  readonly description = 'Assigns or unassigns a role';
+  readonly arguments = [];
+  readonly dependencies = [];
+  readonly handler = this.resolveRole;
+
+  /// Resolve [roleName] to a `Role`; verify that the user can assign it; add it to user, otherwise, remove it
+  async resolveRole(message: Message) {
+    const roleName = message.content.toLowerCase();
+
+    // If the sought role is not found in [allRoles]
+    if (!this.module.allRoles.includes(roleName)) {
+      return;
+    }
+
+    // If the sought role is not a proficiency role
+    if (!roles.proficiency.includes(roleName)) {
+      return this.resolveNonProficiencyRole(message.channel as TextChannel, message.member!, roleName);
+    }
+
+    if (!this.hasRole(message.member!, roleName)) {
+      const currentProficiencyRole = this.getCurrentProficiencyRole(message.member!);
+      return this.addOrReplaceProficiencyRole(message.channel as TextChannel, message.member!, roleName, currentProficiencyRole);
+    }
+
+    let baseMessage = `Your level is already ${
+      this.module.toTag(this.module.findRole(message.member!, roleName).id)
+    }.\n\nInstead, you may `;
+    let upgradeMessage = ':arrow_double_up: upgrade to ';
+    let downgradeMessage = ':arrow_double_down: downgrade to ';
+
+    const indexOfProficiencyRole = roles.proficiency.indexOf(roleName);
+
+    const roleTagsToUpgradeTo: string[] = this.getTagsOfProficiencyRolesBetterThan(message.member!, indexOfProficiencyRole);
+    const roleTagsToDowngradeTo: string[] = this.getTagsOfProficiencyRolesWorseThan(message.member!, indexOfProficiencyRole);
+
+    if (roleTagsToUpgradeTo.length !== 0) {
+      upgradeMessage += Utils.join(roleTagsToUpgradeTo, 'or');
+      baseMessage += upgradeMessage;
+
+      if (roleTagsToDowngradeTo.length !== 0) {
+        baseMessage += ' or ';
+      }
+    }
+
+    if (roleTagsToDowngradeTo.length !== 0) {
+      downgradeMessage += Utils.join(roleTagsToDowngradeTo, 'or');
+      baseMessage += downgradeMessage;
+    }
+
+    Client.info(message.channel as TextChannel, baseMessage);
+  }
+  /// Assign or unassign a non-proficiency role
+  resolveNonProficiencyRole(textChannel: TextChannel, member: GuildMember, roleName: string) {
+    // If the user does not have a proficiency role yet
+    if (!this.module.hasProficiency(member)) {
+      return;
+    }
+
+    // If the user already has the sought role
+    if (this.hasRole(member, roleName)) {
+      return this.removeRole(textChannel, member, roleName);
+    }
+
+    return this.addRole(textChannel, member, roleName);
+  }
+
+  /// Add role to user by name
+  addRole(textChannel: TextChannel, member: GuildMember, roleName: string) {
+    member.roles.add(this.module.findRole(member, roleName));
+
+    const capitalisedRoleName = Utils.capitaliseWords(roleName);
+
+    let message = `You now have the role '${capitalisedRoleName}'`;
+
+    if (roles.ethnicities.includes(roleName)) {
+      if (this.hasEnoughEthnicityRoles(member)) {
+        Client.warn(textChannel, `You may not be of more than ${roles.maximumEthnicityRoles} Romanian ethnicities at any given time`);
+        return;
+      }
+
+      message = `You are now ${capitalisedRoleName}`;
+    }
+
+    if (roles.regions.includes(roleName)) {
+      if (this.hasEnoughRegionRoles(member)) {
+        Client.warn(textChannel, `You may not have more than ${roles.maximumRegionRoles} region roles at any given time`);
+        return;
+      }
+
+      message = `You are now from ${capitalisedRoleName}`;
+    }
+
+    if (roles.abroad.includes(roleName)) {
+      message = `You are now a ${roleName}`;
+    }
+
+    Client.info(textChannel, message + ` :partying_face:`);
+  }
+
+  /// Remove role from user by name
+  removeRole(textChannel: TextChannel, member: GuildMember, roleName: string) {
+    member.roles.remove(this.module.findRole(member, roleName));
+
+    const capitalisedRoleName = Utils.capitaliseWords(roleName);
+
+    let message = `You no longer have the role '${capitalisedRoleName}'`;
+
+    if (roles.regions.includes(roleName)) {
+      message = `You are no longer from ${capitalisedRoleName}`;
+    }
+
+    if (roles.ethnicities.includes(roleName)) {
+      message = `You are no longer ${capitalisedRoleName}`;
+    }
+
+    if (roles.abroad.includes(roleName)) {
+      message = `You are no longer a ${capitalisedRoleName}`;
+    }
+
+    Client.info(textChannel, message + ` :sob:`);
+  }
+
+  /// Assign the user a proficiency role, or if the user already has one,
+  /// replace it with the newly requested proficiency role
+  addOrReplaceProficiencyRole(
+    textChannel: TextChannel, 
+    member: GuildMember, 
+    roleName: string, 
+    currentProficiencyRole: Role | undefined
+  ) {
+    member.roles.add(this.module.findRole(member, roleName));
+
+    if (currentProficiencyRole !== undefined) {
+      this.removeProficiencyRole(member, currentProficiencyRole);
+    }
+
+    Client.info(textChannel, `Your level is now ${roleName}`);
+  }
+
+  /// Remove a proficiency role from the user
+  removeProficiencyRole(member: GuildMember, proficiencyRole: Role) {
+    member.roles.remove(proficiencyRole);
+  }
+
+  /// Check if the user has a role by passing its name
+  hasRole(member: GuildMember, roleName: string): boolean {
+    return member.roles.cache.some((role) => role.name.toLowerCase() === roleName);
+  }
+
+  /// Get the user's existent proficiency role
+  getCurrentProficiencyRole(member: GuildMember): Role | undefined {
+    return member.roles.cache.find((role) => roles.proficiency.includes(role.name.toLowerCase()));
+  }
+
+  /// Get tags of roles which are 'better' than the given role's index
+  getTagsOfProficiencyRolesBetterThan(member: GuildMember, roleIndex: number): string[] {
+    return roles.proficiency.filter(
+      (_, index) => index > roleIndex).map((roleName) => this.module.toTag(this.module.findRole(member, roleName).id)
+    );
+  }
+
+  /// Get tags of roles which are 'worse' than the given role's index
+  getTagsOfProficiencyRolesWorseThan(member: GuildMember, roleIndex: number): string[] {
+    return roles.proficiency.filter(
+      (_, index) => index < roleIndex).map((roleName) => this.module.toTag(this.module.findRole(member, roleName).id)
+    );
+  }
+
+  /// Check if the user already has the maximum number of ethnicity roles
+  hasEnoughEthnicityRoles(member: GuildMember): boolean {
+    return member.roles.cache.filter(
+      (role) => roles.ethnicities.includes(role.name.toLowerCase())
+    ).size >= roles.maximumEthnicityRoles;
+  }
+
+  /// Check if the user already has the maximum number of region roles
+  hasEnoughRegionRoles(member: GuildMember): boolean {
+    return member.roles.cache.filter(
+      (role) => roles.regions.includes(role.name.toLowerCase())
+    ).size >= roles.maximumRegionRoles;
+  }
+}
---
+++ src/modules/roles/commands/assignable.ts
@@ -0,0 +1,35 @@
+import { EmbedField, Message, TextChannel } from "discord.js";
+
+import { Client } from "../../../client/client";
+import { Embed } from "../../../client/embed";
+
+import { Roles } from "../roles";
+import { Command } from "../../command";
+
+import { Utils } from "../../../utils";
+
+import roles from '../../../roles.json';
+
+export class Assignable extends Command<Roles> {
+  readonly identifier = 'roles';
+  readonly aliases = ['rolelist'];
+  readonly description = 'Displays a list of assignable roles';
+  readonly arguments = [];
+  readonly dependencies = [];
+  readonly handler = this.displayAssignableRoles;
+
+  async displayAssignableRoles(message: Message) {
+    const roleCategoriesToDisplay = Object.entries(roles)
+      .slice(2, this.module.hasProficiency(message.member!) ? undefined : 2);
+      
+    const fields = roleCategoriesToDisplay.map<EmbedField>(([key, value]) => {return {
+      name: Utils.capitaliseWords(key),
+      value: (value as string[]).map((roleName) => this.module.toTag(
+        this.module.findRole(message.member!, roleName).id)
+      ).join(' '),
+      inline: true,
+    }});
+
+    Client.send(message.channel as TextChannel, new Embed({fields: fields}));
+  }
+}
