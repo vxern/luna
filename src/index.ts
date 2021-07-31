41ceb097ac557416b31e8ee255d94a0e3825922d
vxern <vxern@wordcollector.co.uk>
2021-07-31 23:34:23 +0100

    I don't like unencapsulated code, so I moved it into `main`

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,21 @@
-import { LunaClient } from './client/client';
-
-const requiredEnv = ['DISCORD_SECRET', 'FAUNA_SECRET', 'YOUTUBE_SECRET'];
-/// Array of booleans indicating which environment variables are present at launch
-const presentEnv = requiredEnv.map((env) => env in process.env);
-
-// If at least one environment variable is not present
-if (presentEnv.includes(false)) {
-  const missingEnv = presentEnv.filter((env: boolean) => !env).map((_, index) => requiredEnv[index]);
-  console.error(`Missing one or more required environment variables: ${missingEnv.join(', ')}`);
-  process.exit(1);
-}
-
-const Client: LunaClient = new LunaClient();
+import { Client } from './client/client';
 
 async function main() {
-  await Client.login();
+  /// Array of environment variables required to run the program
+  const requiredEnv = ['DISCORD_SECRET', 'FAUNA_SECRET', 'YOUTUBE_SECRET'];
+  /// Array of booleans indicating which environment variables are present at launch
+  const presentEnv = requiredEnv.map((env) => env in process.env);
+  
+  // If at least one environment variable is not present
+  if (presentEnv.includes(false)) {
+    const missingEnv = presentEnv.filter((env: boolean) => !env).map((_, index) => requiredEnv[index]);
+    console.error(`Missing one or more required environment variables: ${missingEnv.join(', ')}`);
+    process.exit(1);
+  }
+
+  const client: Client = new Client();
+  
+  await client.initialise();
 }
 
 main().catch((error) => console.error(error));
