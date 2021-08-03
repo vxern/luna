9ae01679c34dd4b538e531443328b6b565f3479b
vxern <vxern@wordcollector.co.uk>
2021-08-03 18:38:29 +0100

    Added `resolveBannedUser` and `findBanReason` functions

--- src/modules/moderation/moderation.ts
+++ src/modules/moderation/moderation.ts
@@ -1,4 +1,5 @@
-import { Guild, GuildMember, Message, TextChannel, User } from 'discord.js';
+import { Guild, GuildMember, Message, TextChannel, User, Util } from 'discord.js';
+import * as string from 'string-sanitizer';
 
 import { Client } from '../../client/client';
 
@@ -18,26 +19,17 @@ import roles from '../../roles.json';
 import { Utils } from '../../utils';
 
 const userTag = /<@!?.+>/g;
-const usernameWithDiscriminator = /.+#\d{4}/g;
+const fullTag = /.+#\d{4}/g;
 
 export class Moderation extends Module {
   readonly requirement = (message: Message) => message.member?.roles.cache.map((role) => role.name).includes(Utils.capitaliseWords(roles.moderator)) || false;
   readonly commandsRestricted = Utils.instantiate([Ban, Kick, Mute, Pardon, Unban, Unmute, Warn], [this]);
   readonly commandUnrestricted = Utils.instantiate([CiteRule], [this]);
 
-  /// TODO: Fix this function of doom once and for all
-  ///
-  /// Takes an identifier in the form of a tag, a username or an ID
-  async resolve(type: 'GuildMember' | 'User', message: Message): Promise<GuildMember | User | undefined> {
-    const collection: (GuildMember | User)[] = type === 'User' ? 
-      Array.from(((await message.guild!.fetchBans()).mapValues((banData) => banData.user)).values()) : 
-      Array.from((await message.guild!.members.fetch()).values());
-
-    function getUser(member: GuildMember | User): User {
-      if (member instanceof GuildMember) return member.user;
-
-      return member;
-    }
+  /// Takes an identifier in the form of a full tag, a username or an ID and
+  /// finds the member bearing it
+  async resolveMember(message: Message): Promise<GuildMember | undefined> {
+    const members = await message.guild!.members.fetch();
 
     // If the identifier is a tag, convert it to an ID
     if (userTag.test(message.content)) {
@@ -46,31 +38,76 @@ export class Moderation extends Module {
 
     // If the identifier is an ID
     if (Utils.isNumber(message.content)) {
-      return collection.find((member) => member.id === message.content);
+      return members.find((member) => member.id === message.content);
     }
 
-    // If the username contains a discriminator ( a 4-digit number seen in a tag )
-    if (usernameWithDiscriminator.test(message.content)) {
-      return collection.find((member) => getUser(member).tag === message.content);
+    // If the username is a valid tag ( a username with a discriminator )
+    if (fullTag.test(message.content)) {
+      return members.find((member) => member.user.tag === message.content);
     }
 
-    const usernameLowercase = message.content.toLowerCase();
-    const membersFound = collection.filter(
-      (member) => getUser(member).username.toLowerCase().includes(usernameLowercase)
-    );
-    console.log(membersFound.length);
-    console.log(collection.map((member) => getUser(member).username).filter((username) => username.includes(usernameLowercase)));
-    
-    if (membersFound.length === 0) {
-      Client.warn(message.channel as TextChannel, `No member with the username '${usernameLowercase}' found.`);
+    const membersFound = members.filter((member) => Utils.includes(member.displayName, message.content));
+
+    if (membersFound.size === 0) {
+      Client.warn(message.channel as TextChannel, `No member with the username '${message.content}' found.`);
       return;
     }
 
     const member = await this.browse(
-      message, Array.from(membersFound.values()), (member) => `${getUser(member).tag}, ID \`${getUser(member).id}\``
+      message, Array.from(membersFound.values()), (member) => `${member.user.tag}, ID \`${member.user.id}\``
     );
 
     return member;
+  }
+
+  /// Takes an identifier in the form of an ID, a full tag or a username
+  /// and finds the banned user bearing it
+  async resolveBannedUser(message: Message): Promise<{user: User, reason: string} | undefined> {
+    const bans = Array.from((await message.guild!.fetchBans()).values());
+
+    // If the identifier is an ID
+    if (Utils.isNumber(message.content)) {
+      return bans.find((data) => data.user.id === message.content);
+    }
+
+    // If the username is a valid tag ( a username with a discrimination )
+    if (fullTag.test(message.content)) {
+      return bans.find((data) => data.user.tag === message.content);
+    }
+
+    const usersFound = bans.filter((ban) => Utils.includes(ban.user.username, message.content));
+
+    if (usersFound.length === 0) {
+      Client.warn(message.channel as TextChannel, `No banned users with the username '${message.content}' found.`);
+      return;
+    }
+
+    const user = await this.browse(
+      message, 
+      usersFound, 
+      (data) => `${data.user.tag} ~ banned for: ${this.findBanReason(bans.map((ban) => ban.user), data)}`
+    );
+
+    return user;
+  }
+
+  /// Returns the ban reason or attempts to synthesise it if one does not exist
+  findBanReason(bannedUsers: User[], data: {user: User, reason: string | null}) {
+    if (data.reason !== null) return data.reason;
+
+    const bannedUsersWithSimilarName = bannedUsers.filter(
+      (user) => Utils.areSimilar(user.username, data.user.username)
+    ).length;
+
+    if (bannedUsersWithSimilarName > 5) {
+      return 'Large number of duplicate accounts';
+    }
+
+    if (bannedUsersWithSimilarName > 1) {
+      return 'Duplicate account';
+    }
+
+    return 'No reason found.';
   }
 
   async isMemberBanned(member: GuildMember) {
