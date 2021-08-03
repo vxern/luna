942410d4c34cd0e95d959722e998f525818a62d5
vxern <vxern@wordcollector.co.uk>
2021-08-03 17:20:33 +0100

    `resolve` can now search for banned users, as well as active members

--- src/modules/moderation/moderation.ts
+++ src/modules/moderation/moderation.ts
@@ -1,10 +1,8 @@
-import { Guild, GuildMember, Message, TextChannel } from 'discord.js';
+import { Guild, GuildMember, Message, TextChannel, User } from 'discord.js';
 
 import { Client } from '../../client/client';
 
 import { Module } from '../module';
-
-import { Utils } from '../../utils';
 
 import { Ban } from './commands/ban';
 import { CiteRule } from './commands/cite-rule';
@@ -17,6 +15,8 @@ import { Warn } from './commands/warn';
 
 import roles from '../../roles.json';
 
+import { Utils } from '../../utils';
+
 const userTag = /<@!?.+>/g;
 const usernameWithDiscriminator = /.+#\d{4}/g;
 
@@ -25,9 +25,19 @@ export class Moderation extends Module {
   readonly commandsRestricted = Utils.instantiate([Ban, Kick, Mute, Pardon, Unban, Unmute, Warn], [this]);
   readonly commandUnrestricted = Utils.instantiate([CiteRule], [this]);
 
+  /// TODO: Fix this function of doom once and for all
+  ///
   /// Takes an identifier in the form of a tag, a username or an ID
-  async resolveMember(message: Message): Promise<GuildMember | undefined> {
-    const members = await message.guild!.members.fetch();
+  async resolve(type: 'GuildMember' | 'User', message: Message): Promise<GuildMember | User | undefined> {
+    const collection: (GuildMember | User)[] = type === 'User' ? 
+      Array.from(((await message.guild!.fetchBans()).mapValues((banData) => banData.user)).values()) : 
+      Array.from((await message.guild!.members.fetch()).values());
+
+    function getUser(member: GuildMember | User): User {
+      if (member instanceof GuildMember) return member.user;
+
+      return member;
+    }
 
     // If the identifier is a tag, convert it to an ID
     if (userTag.test(message.content)) {
@@ -36,26 +46,39 @@ export class Moderation extends Module {
 
     // If the identifier is an ID
     if (Utils.isNumber(message.content)) {
-      return members.find((member) => member.id === message.content);
+      return collection.find((member) => member.id === message.content);
     }
 
-    // If the character at 
+    // If the username contains a discriminator ( a 4-digit number seen in a tag )
     if (usernameWithDiscriminator.test(message.content)) {
-      return members.find((member) => member.user.tag === message.content);
+      return collection.find((member) => getUser(member).tag === message.content);
     }
 
     const usernameLowercase = message.content.toLowerCase();
-    const membersFound = members.filter((member) => member.user.username.toLowerCase().includes(usernameLowercase));
-
-    if (membersFound === undefined) {
-      Client.warn(message.channel as TextChannel, `No user with the username '${usernameLowercase}' found.`);
+    const membersFound = collection.filter(
+      (member) => getUser(member).username.toLowerCase().includes(usernameLowercase)
+    );
+    console.log(membersFound.length);
+    console.log(collection.map((member) => getUser(member).username).filter((username) => username.includes(usernameLowercase)));
+    
+    if (membersFound.length === 0) {
+      Client.warn(message.channel as TextChannel, `No member with the username '${usernameLowercase}' found.`);
       return;
     }
 
     const member = await this.browse(
-      message, Array.from(membersFound.values()), (member) => `${member.user.tag}, ID \`${member.user.id}\``
+      message, Array.from(membersFound.values()), (member) => `${getUser(member).tag}, ID \`${getUser(member).id}\``
     );
 
     return member;
+  }
+
+  async isMemberBanned(member: GuildMember) {
+    const bans = await member?.guild.fetchBans();
+    return Array.from(bans?.keys()).includes(member.id);
+  }
+
+  async fetchBannedUsers(guild: Guild) {
+    return (await guild.fetchBans()).map((banObject) => banObject.user);
   }
 }
