4bbe73c25b20a8e6c09a709f8ae1a8389e234001
vxern <vxern@wordcollector.co.uk>
2021-08-05 01:04:14 +0100

    Added `pardon` command for removing previously attributed warnings

--- src/modules/moderation/commands/pardon.ts
+++ src/modules/moderation/commands/pardon.ts
@@ -1,11 +1,64 @@
+import { Client } from "../../../client/client";
+
 import { Moderation } from "../moderation";
-import { Command } from "../../command";
+import { Command, HandlingData } from "../../command";
+
+import { Utils } from "../../../utils";
 
 export class Pardon extends Command<Moderation> {
   readonly identifier = 'pardon';
   readonly aliases = [];
   readonly description = `Pardons a user by removing a warning they've received`;
-  readonly arguments = ['tag | name | id', 'warning id'];
+  readonly parameters = ['identifier', 'id'];
   readonly dependencies = [];
-  readonly handler = this.module.displayUnimplemented;
+  readonly handler = this.pardon;
+
+  async pardon({message, parameters}: HandlingData) {
+    const member = await this.module.resolveMember(message, parameters.get('identifier')!);
+
+    if (member === undefined) {
+      return;
+    }
+
+    if (!member.bannable) {
+      Client.warn(message.channel, 'You do not have the authority to pardon this member.');
+      return;
+    }
+
+    Client.database.fetchDatabaseEntryOrCreate(message.channel, member.user).then((target) => {
+      if (target === undefined) {
+        return;
+      }
+
+      const numberOfWarnings = Object.keys(target.user.warnings).length - 1;
+
+      if (numberOfWarnings === -1) {
+        Client.warn(message.channel, `${member.user.tag} has no warnings, and thus it is not possible to pardon them.`);
+        return;
+      }
+
+      const warningId = parameters.get('id')!;
+
+      if (!Utils.isNumber(warningId)) {
+        Client.warn(message.channel, 'A warning ID must be a number.');
+        return;
+      }
+
+      if (!Object.keys(target.user.warnings).includes(warningId)) {
+        Client.warn(message.channel, `${member.user.tag} does not have a warning with an ID of ${warningId}.`);
+        return;
+      }
+
+      const warningMessage = target.user.warnings[warningId][0];
+
+      delete target.user.warnings[warningId];
+
+      Client.database.update(target);
+
+      Client.info(message.channel, 
+        `**${member.user.tag}** has been pardoned from warning no. ${warningId}: ${warningMessage}\n\n` +
+        `**${member.user.tag}** now has ${Utils.pluralise('warning', numberOfWarnings)}.`
+      );
+    });
+  }
 }
