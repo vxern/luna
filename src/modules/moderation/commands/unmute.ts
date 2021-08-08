3292ffe01a3cea92457149324f326db6baa04b6c
vxern <vxern@wordcollector.co.uk>
2021-08-08 12:38:51 +0100

    Added `Unmute` command

--- src/modules/moderation/commands/unmute.ts
+++ src/modules/moderation/commands/unmute.ts
@@ -1,5 +1,7 @@
+import { Client } from "../../../client/client";
+
 import { Moderation } from "../moderation";
-import { Command } from "../../command";
+import { Command, HandlingData } from "../../command";
 
 export class Unmute extends Command<Moderation> {
   readonly identifier = 'unmute';
@@ -7,5 +9,30 @@ export class Unmute extends Command<Moderation> {
   readonly description = 'Unmutes a user, allowing them access to the server again';
   readonly parameters = ['identifier'];
   readonly dependencies = [];
-  readonly handler = this.module.displayUnimplemented;
+  readonly handler = this.unmute;
+
+  async unmute({message, parameter}: HandlingData) {
+    const member = await Moderation.resolveMember(message, parameter!);
+
+    if (member === undefined) return;
+
+    if (!member.bannable) {
+      Client.warn(message.channel, 'You do not have the authority to mute this member.');
+      return;
+    }
+
+    Client.database.fetchOrCreateDocument(member.user).then((document) => {
+      if (!document.user.mute) {
+        Client.warn(message.channel, `**${member.user.tag}** is not muted.`);
+        return;
+      }
+
+      // Hijack the expire callback, and cancel the timeout which was due to execute it
+      const [timeout, expire] = Client.database.muteTimeouts.get(member.user.id)!;
+      clearTimeout(timeout);
+      expire();
+
+      Client.tip(message.channel, `**${member.user.tag}** has been unmuted.`);
+    });
+  }
 }
