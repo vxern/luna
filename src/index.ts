ac0f1083a4e6c222cbe3807c27c08e8872ac7c39
vxern <vxern@wordcollector.co.uk>
2021-08-11 23:57:57 +0100

    Fixed the wrong environment variables being listed as missing

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import { Client } from './client/client';
 
 async function main() {
-  /// Array of environment variables required to run the program
+  // Array of environment variables required to run the program
   const requiredEnv = ['DISCORD_SECRET', 'FAUNA_SECRET', 'YOUTUBE_SECRET'];
-  /// Array of booleans indicating which environment variables are present at launch
+  // Array of booleans indicating which environment variables are present at launch
   const presentEnv = requiredEnv.map((env) => env in process.env);
   
   // If at least one environment variable is not present
   if (presentEnv.includes(false)) {
-    const missingEnv = presentEnv.filter((env: boolean) => !env).map((_, index) => requiredEnv[index]);
+    const missingEnv = requiredEnv.filter((_, index) => !presentEnv[index]);
     console.error(`Missing one or more required environment variables: ${missingEnv.join(', ')}`);
     process.exit(1);
   }
