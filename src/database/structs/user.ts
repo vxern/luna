a833ea037f9a1ffb2c6a6286d662cdae33ab2f74
vxern <vxern@wordcollector.co.uk>
2021-08-15 17:12:19 +0100

    Added new fields for praising users and rewarding them with emojis

--- src/database/structs/user.ts
+++ src/database/structs/user.ts
@@ -1,10 +1,22 @@
+
+import moment, { Moment } from "moment";
+
+import { Praise } from "./praise";
 import { Warning } from "./warning";
+
+import { EmojiCollection } from "../../modules/social/social";
 
 interface UserData {
   username: string;
   id: string;
+
   warnings: Warning[];
   mute?: Warning;
+
+  cannotPraiseUntil?: Moment;
+  praises: Praise[];
+  chosenEmojiSet?: string;
+  isHiddenEmoji: boolean;
 }
 
 export class User {
@@ -14,11 +26,20 @@ export class User {
   warnings: (Warning | null)[];
   mute?: Warning | null;
 
+  cannotPraiseUntil?: Moment;
+  praises: Praise[];
+  chosenEmojiSet?: EmojiCollection;
+  isHiddenEmoji: boolean;
+
   constructor(data: UserData) {
     this.username = data.username;
     this.id = data.id;
     this.warnings = data.warnings;
     this.mute = data.mute;
+    this.cannotPraiseUntil = data.cannotPraiseUntil;
+    this.praises = data.praises;
+    this.chosenEmojiSet = data.chosenEmojiSet as EmojiCollection | undefined;
+    this.isHiddenEmoji = data.isHiddenEmoji;
   }
 
   serialize() {
@@ -27,6 +48,10 @@ export class User {
       id: this.id,
       warnings: this.warnings.map((warning) => warning && warning.serialize()),
       mute: this.mute && this.mute.serialize(),
+      cannotPraiseUntil: this.cannotPraiseUntil?.unix(),
+      praises: this.praises.map((praise) => praise.serialize()),
+      chosenEmojiSet: this.chosenEmojiSet,
+      isHiddenEmoji: this.isHiddenEmoji,
     };
 
     // Remove null entries after serialization
@@ -42,6 +67,10 @@ export class User {
       id: faunaObject.id,
       warnings: faunaObject.warnings.map((warning: any) => Warning.deserialize(warning)),
       mute: faunaObject.mute && Warning.deserialize(faunaObject.mute),
+      cannotPraiseUntil: faunaObject.cannotPraiseUntil && moment.unix(faunaObject.cannotPraiseUntil),
+      praises: faunaObject.praises.map((praise: any) => Praise.deserialize(praise)),
+      chosenEmojiSet: faunaObject.chosenEmojiSet,
+      isHiddenEmoji: faunaObject.isHiddenEmoji || false,
     });
   }
 }
