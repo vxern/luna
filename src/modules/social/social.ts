e344bc5159f84f346a604bbe01789b423f739289
vxern <vxern@wordcollector.co.uk>
2021-08-15 22:20:33 +0100

    Extended social module with emoji assignment

--- src/modules/social/social.ts
+++ src/modules/social/social.ts
@@ -1,11 +1,60 @@
+import { GuildMember, PartialGuildMember } from 'discord.js';
+
 import { GuildMessage } from '../../client/client';
 
+import { Document } from "../../database/structs/document";
+
+import { Praise } from './commands/praise';
 import { Module } from '../module';
 
 import { Utils } from '../../utils';
 
+import config from '../../config.json';
+
+export type EmojiCollection = 'flowers' | 'plants' | 'stars';
+
 export class Social extends Module {
-  readonly requirement = (message: GuildMessage) => Utils.isModerator(message);
+  readonly requirement = (message: GuildMessage) => Utils.isModerator(message.member!);
   readonly commandsRestricted = Utils.instantiate([], [this]);
-  readonly commandUnrestricted = Utils.instantiate([], [this]);
+  readonly commandUnrestricted = Utils.instantiate([Praise], [this]);
+
+  private static readonly tierThresholds = [5, 10, 20, 40, 100];
+  private static readonly emojiCollections = {
+    flowers:  ['🌸', '💮', '🌷', '🌹', '💐'],
+    medals:   ['🥉', '🥈', '🥇', '🎖️', '🏆'],
+    plants:   ['🌱', '🌿', '🍀', '🍁', '🌾'],
+    stars:    ['✨', '⭐', '🌟', '💫', '🌠'],
+    vehicles: ['🛴', '🚲', '🛵', '🏍️', '🏎️'],
+  };
+
+  static assignEmoji(document: Document, member: GuildMember) {
+    // The threshold array is reversed to simulate finding from the back, rather than from the front
+    const tierIndex = this.tierThresholds
+      .reverse()
+      .findIndex((threshold) => document.user.praises.length >= threshold)!;
+    const collection = (!!document.user.chosenEmojiSet ? 
+      Social.emojiCollections[document.user.chosenEmojiSet] : 
+      Social.emojiCollections['flowers']).reverse();
+
+    const nicknameWithoutEmoji = member.displayName.includes('︱') ?
+      member.displayName.split('︱').slice(1).join('︱') :
+      member.displayName;
+
+    const newNickname = (
+      !document.user.isHiddenEmoji && tierIndex !== -1 ? 
+      collection[tierIndex] + '︱' : 
+      ''
+    ) + nicknameWithoutEmoji;
+
+    member.setNickname(newNickname);
+  }
+
+  static displayNumberOfPraises(document: Document): string {
+    return `${Utils.toUserTag(document.user.id)} now has ${Utils.pluralise('praise', document.user.praises.length)}.\n\n`;
+  }
+
+  static displayEmojiInstructions(): string {
+    return 'For instructions on how to choose a different emoji collection or how to hide the acknowledgement emoji, ' + 
+      `use ${Utils.toCode(`${config.alias} usage profile`)}`;
+  }
 }
